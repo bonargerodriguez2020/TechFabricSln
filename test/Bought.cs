8f1af922b5b27a04dcb1b693a153f75e4490a62b
bonargerodriguez2020 <71228662+bonargerodriguez2020@users.noreply.github.com>
2021-10-13 20:11:14 -0500

    Add files

---
+++ test/Bought.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Colections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechFabricSln
+{ 
+public class Bought
+{
+	public User MadeBy { get; get; }
+	
+	public bool IsBoughBy(User user)
+	{
+			if (user.customer)
+				return true;
+			if (MadeBy == user)
+				return true;
+
+			return false;
+	}
+
+}
+public class User
+{
+	public bool customer { get; get; }
+}
+}
